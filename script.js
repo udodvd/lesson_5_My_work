6170997c11283f6d73443f7d6b08eabd954f12a4
udodvd <Podliza245>
2017-04-12 21:10:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// alert('hello!!')
+// function getRand(min,max) {
+//     return Math.floor(Math.random() * (max-min))+ min;
+// }
+// function  getRandom(start,end) {
+//     var res = [];
+// }
+// var result = sum(1,3);
+// console.log(result);
+//
+// function  sum(a,b) {
+//    return a+b;
+// }
+// var arr = [1,2,3,4,5];
+// arr.forEach(function (el) {
+//     console.log(el);
+// })
+// var arr = [1,2,3,4,5];
+// function  myFoo() {
+//     var arr
+// }
+// var obj = {
+//     property: true;
+//     someMethod: function () {
+//         console.log(123456);
+//     }
+// }
+// function counter() {
+//     var count = 0;
+//     var obj = {
+//         inc:function () {
+//             count +=1;
+//         },
+//         dec:function () {
+//             count -=1;
+//         },
+//         get:function () {
+//             return count;
+//         }
+//     };
+//     return obj;
+// };
+// var countetreInst = counter();
+// countetreInst.inc()
+// countetreInst.inc()
+// console.log(countetreInst.inc());
+// console.log(countetreInst.get());
+// console.log(countetreInst.dec());
